d23fc1ffe5b8083ccf48317c3c43e5581bff01e6
Antonio Roberto <juniormach96@gmail.com>
2024-01-30 17:28:28 -0500

    update app.py with all stacks

--- app.py
+++ app.py
@@ -1,12 +1,46 @@
 #!/usr/bin/env python3
 import aws_cdk as cdk
 
-from cdk.etl_lambda_s3 import ETLLambdaS3
+from cdk.codebuild_stack import CodeBuildStack
+from cdk.etl_stack import ETLStack
+from cdk.source_stack import SourceStack
+from cdk.train_ml_stack import TrainMLStack
 
 app = cdk.App()
-ETLLambdaS3(
+
+source_stack = SourceStack(app, "SourceStack")
+
+s3_bucket = source_stack.s3_bucket
+
+ecr_repositories = {
+    "etl": source_stack.etl_ecr_repository,
+    "train_ml": s3_bucket.train_ml_ecr_repository,
+}
+
+etl_stack = ETLStack(
     app,
-    "ETLLambdaS3",
+    "ETLStack",
+    s3_bucket=s3_bucket,
+    etl_ecr_repository=ecr_repositories["etl"],
+)
+
+train_ml_stack = TrainMLStack(
+    app,
+    "TrainMLStack",
+    s3_bucket=etl_stack.bucket,
+    train_ml_ecr_repository=ecr_repositories["train_ml"],
+)
+
+lambda_functions = {
+    "etl": etl_stack.etl_lambda_function,
+    "train_ml": train_ml_stack.train_ml_lambda_function,
+}
+
+codebuild_stack = CodeBuildStack(
+    app,
+    "CodeBuildStack",
+    ecr_repositories=ecr_repositories,
+    lambda_functions=lambda_functions,
 )
 
 app.synth()
