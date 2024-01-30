f879360fcfe55aa6b8d47a289bd0ef77264877cc
Antonio Roberto <juniormach96@gmail.com>
2024-01-30 10:49:12 -0500

    change load functions

--- functions/ETL/scripts/load.py
+++ functions/ETL/scripts/load.py
@@ -8,7 +8,7 @@ from prefect import task
 
 
 @task
-def load(dfs: List[pd.DataFrame]) -> None:
+def load(df: pd.DataFrame, file_name: str) -> None:
     s3_bucket = os.environ.get("S3_BUCKET")
     if not s3_bucket:
         raise ValueError("S3_BUCKET environment variable not set")
@@ -17,17 +17,10 @@ def load(dfs: List[pd.DataFrame]) -> None:
 
     # Convert DataFrame to CSV in memory
     csv_buffer_games = StringIO()
-    dfs[0].to_csv(csv_buffer_games, index=False)
+    df.to_csv(csv_buffer_games, index=False)
     csv_buffer_games.seek(0)  # Rewind the buffer
-
-    csv_buffer_to_predict = StringIO()
-    dfs[1].to_csv(csv_buffer_to_predict, index=False)
-    csv_buffer_to_predict.seek(0)  # Rewind the buffer
 
     # Upload to S3
     s3_client.put_object(
-        Bucket=s3_bucket, Key="games.csv", Body=csv_buffer_games.getvalue()
-    )
-    s3_client.put_object(
-        Bucket=s3_bucket, Key="to_predict.csv", Body=csv_buffer_to_predict.getvalue()
+        Bucket=s3_bucket, Key=f"{file_name}.csv", Body=csv_buffer_games.getvalue()
     )
