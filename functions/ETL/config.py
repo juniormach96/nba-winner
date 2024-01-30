359c390d805df6b3c78190191b313c02dfbb57b6
Antonio Roberto <juniormach96@gmail.com>
2024-01-30 10:50:10 -0500

    update config

--- functions/ETL/config.py
+++ functions/ETL/config.py
@@ -1,21 +1,29 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from datetime import date
+from typing import List
 
 
 @dataclass
 class AppConfig:
-    START_DATE: str = date.today().strftime("%m/%d/%Y")
-    NBA_LEAGUE_ID: str = "00"
-    PRE_SELECTED_FEATURES: list = [
-        "TEAM_NAME",
-        "GAME_ID",
-        "GAME_DATE",
-        "PTS",
-        "PLUS_MINUS",
-        "MATCHUP",
-    ]
+    START_DATE: str = "01/01/2020"
+    # NBA_LEAGUE_ID: str = "00"
+    BASE_NBA_URL = "https://www.balldontlie.io/api/v1/games"
+    PRE_SELECTED_FEATURES: List[str] = field(
+        default_factory=lambda: [
+            "id",
+            "date",
+            "home_team_abbreviation",
+            "visitor_team_abbreviation",
+            "home_team_score",
+            "visitor_team_score",
+        ]
+    )
     MIN_GAMES_PER_TEAM: int = 100
-    MOVING_AVERAGE_WINDOWS: list = [1, 5, 10, 15, 20]
+    MOVING_AVERAGE_WINDOWS: List[int] = field(
+        default_factory=lambda: [1, 5, 10, 15, 20]
+    )
+    TEAM_NAME_COLUMN: str = "team_abbreviation"
+    OPPONENT_TEAM_COLUMN: str = "opponent_team_abbreviation"
 
 
 config = AppConfig()
