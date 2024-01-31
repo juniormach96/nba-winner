1fd617a1255fef6dcabde607a83b56047b55b918
Antonio Roberto <juniormach96@gmail.com>
2024-01-31 05:39:55 -0500

    add more variables to train_ml config

--- functions/train_ml_model/config.py
+++ functions/train_ml_model/config.py
@@ -1,5 +1,6 @@
 import os
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import List
 
 
 @dataclass
@@ -7,6 +8,22 @@ class AppConfig:
     S3_BUCKET: str = os.getenv("S3_BUCKET")
     TRAIN_FILE_NAME: str = os.getenv("TRAIN_FILE_NAME")
     ML_MODEL_FILE: str = os.getenv("ML_MODEL_FILE")
+    FEATURES: List[str] = field(
+        default_factory=lambda: [
+            "home_avg_last_5_team_score",
+            "home_avg_last_5_opponent_score",
+            "home_avg_last_10_team_score",
+            "home_avg_last_10_opponent_score",
+            "home_avg_last_15_team_score",
+            "home_avg_last_15_opponent_score",
+            "home_avg_last_20_team_score",
+            "home_avg_last_20_opponent_score",
+        ]
+    )
+    N_CALLS_OPTIMIZATION: int = 20
+    TARGET: List[str] = field(
+        default_factory=lambda: ["home_team_score", "away_team_score"]
+    )
 
 
 config = AppConfig()
